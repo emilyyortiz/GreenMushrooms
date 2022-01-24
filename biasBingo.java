ebe94896e6af8c986b5609d3ddebcd28e6f24d8d
Kartik <kvanjani20@stuy.edu>
2022-01-23 21:58:33 -0500

    updated

--- biasBingo.java
+++ biasBingo.java
@@ -9,36 +9,70 @@ public class biasBingo extends BingoGame {
 
   private static BingoCard Card = new BingoCard();
 
-  // draws a ball from the upper or lower half ballBlower
-  public static int drawBall(int[] Blower, int drawnBall) {
-    System.out.print("Type 0 to draw a ball from the lower half, and any other key to draw from the upper half: ");
-    int  half; 
-    int random = (int)Blower[((int)(Math.random() * 50))];
+// draws a ball from the upper or lower half ballBlower -Tasnim
+  public static Object[] drawBall(int[] Blower, int drawnBall) { //- Tasnim
+
+    //choosing
+    System.out.println("Type 0 to draw a ball from the lower half.");
+    System.out.println("Type 1 to draw a ball from the upper half.\n");
+    System.out.println("Type 2 to draw a ball randomly.");
+    System.out.println("Lower, Upper, or Random? : ");
+    String s = in.nextLine();
+    int half = -1;
+
+    try {
+      if (s.equals("0")) {
+         half = 0;
+      }
+      if (s.equals("1")) {
+         half = 1;
+      }
+    if (s.equals("2")) {
+       half = 2;
+      }
+    }
+    catch (Exception e) {
+      System.out.println("Please type a valid input!");
+    }
+
+    System.out.println("\ndrawing ball...\n");
+    tools.wait(500);
+
+    int random = -1;
+    while (random <= -1) {
+      if (half == 2) {
+        random = (int)Blower[((int)(Math.random() * Blower.length-1))]; //normal
+      }
+      if (half == 0) {
+        random = (int)Blower[((int)(Math.random() * ((Blower.length-1)/2) ))]; //lower
+      }
+      if (half == 1) {
+        random = (int)Blower[((int)(Math.random() * ((Blower.length-1)/2 + 50) ))]; //upper
+      }
+    }
+
     if (random > -1) {
       drawnBall = random;
-      if (half != 0){
-        random += 50;
-      }
-     
+      int i = tools.linSearchint(Blower, random);
+      Blower[i] = -1;
+      Blower = tools.selectionSortint(Blower);
       System.out.print("You drew a "+ drawnBall + " ball!\n");
-    } else {
-      drawBall(Blower, drawnBall);
-    
- 
-   return new drawnBall 
-	return new Blower 
-  }
+    }
 
-  private void bingoTurn() {
+    System.out.println();
+    return new Object[]{drawnBall, Blower};
+  }
+public static void ballDrawn ( 
     ballDrawn = drawBall(ballBlower, ballDrawn);
     bingoCard = Card.markX(bingoCard, ballDrawn);
     turn_num++;
     Card.printCard( bingoCard);
     Card.printBlower( ballBlower);
     System.out.print("\nPress enter to continue:");
-  }
-
-
-
-
+  )
 }
+
+
+
+
+
